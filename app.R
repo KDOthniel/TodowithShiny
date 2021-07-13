2d6548dbd3b8512c9df7352545e0acae54e854d1
Othniel <kpoviessi.doheto.othniel@e.mbox.nagoya-u.ac.jp>
2021-07-13 21:14:44 +0900

    end 1st paradigme

--- app.R
+++ app.R
@@ -1,15 +1,25 @@
 library(shiny)
+library(gapminder)
+data("gapminder")
 
-ui<-fluidPage(
-    
-)
+
+
+ui<-fluidPage(h1("My Shiny app"),
+              h3("I love doing this"),
+              "If you don't want to learn you are dead",
+              br(),
+              strong("Hello World!"),
+  titlePanel("My little world"),
+  sidebarLayout(sidebarPanel("This is a side panel"),
+                mainPanel("And this is the main Stuff")))
+
 server<-function(input,output){
-    
+  
 }
 
 shinyApp(ui=ui, server = server)
 
-runApp()
+
 
 
 
