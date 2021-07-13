edcf6ae1aa005489019111356f28126c125d6f9e
Othniel <kpoviessi.doheto.othniel@e.mbox.nagoya-u.ac.jp>
2021-07-13 21:49:10 +0900

    Before going further

--- app.R
+++ app.R
@@ -4,14 +4,14 @@ data("gapminder")
 
 
 
-ui<-fluidPage(h1("My Shiny app"),
-              h3("I love doing this"),
-              "If you don't want to learn you are dead",
-              br(),
-              strong("Hello World!"),
-  titlePanel("My little world"),
-  sidebarLayout(sidebarPanel("This is a side panel"),
-                mainPanel("And this is the main Stuff")))
+ui<-fluidPage( em("Add several more strings to fluidPage and run the app.
+               Nothing too exciting is happening yet, but you should just see all 
+               the text in one contiguous block."),
+               
+  titlePanel("Welcome to the Gapminder Shiny App!"),
+  
+  sidebarLayout(sidebarPanel("This is a our side Panel"),
+                mainPanel(strong("And this is the main Stuff!"))))
 
 server<-function(input,output){
   
